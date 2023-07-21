49b581bc33251ed6113ae5b4bd5e31cda44cd093
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-07-21 15:37:20 -0300

    add yarn dependencies tree

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,9 @@
 import { TodoItem } from './TodoItem'
+import { useState } from 'react'
 import { useTodo } from '../context'
 import { SiStarship } from 'react-icons/si'
 
 export const TodoList = () => {
+  const [todos, setTodos] = useState<string[]>([])
   return <div>TodoList</div>
 }
